07852b55dd88b0ab1f726e753551f76c0d7615a3
Keks <keks@htmlacademy.ru>
2024-12-11 18:18:55 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/render-comments.js
@@ -0,0 +1,53 @@
+const commentsList = document.querySelector('.social__comments');
+const commentTemplate = commentsList.querySelector('.social__comment');
+const commentCount = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.social__comment-loader');
+
+const VISIBLE_COMMENTS = 5;
+let shownComments = 0;
+let comments = [];
+
+commentsList.innerHTML = '';
+
+const renderNextComments = () => {
+  const commentsFragment = document.createDocumentFragment();
+  const renderedComments = comments.slice(shownComments, shownComments + VISIBLE_COMMENTS);
+  const renderedCommentsLength = renderedComments.length + shownComments;
+
+  renderedComments.forEach ((comment) => {
+    const pictureComment = commentTemplate.cloneNode(true);
+
+    pictureComment.querySelector('.social__picture').src = comment.avatar;
+    pictureComment.querySelector('.social__picture').alt = comment.name;
+    pictureComment.querySelector('.social__text').textContent = comment.message;
+
+    commentsFragment.appendChild(pictureComment);
+  });
+
+  commentsList.appendChild(commentsFragment);
+
+  commentCount.firstChild.textContent = `${renderedCommentsLength} из `;
+  commentCount.querySelector('.comments-count').textContent = comments.length;
+
+  if (renderedCommentsLength >= comments.length) {
+    commentsLoader.classList.add('hidden');
+  }
+
+  shownComments += VISIBLE_COMMENTS;
+};
+
+const clearComments = () => {
+  shownComments = 0;
+  commentsList.innerHTML = '';
+  commentsLoader.classList.remove('hidden');
+  commentsLoader.removeEventListener('click', renderNextComments);
+};
+
+const renderComments = (currentPhotoComments) => {
+  comments = currentPhotoComments;
+  renderNextComments();
+
+  commentsLoader.addEventListener('click', renderNextComments);
+};
+
+export {clearComments, renderComments};
