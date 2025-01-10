99b839f3e65dca36688fa170b91236c4b891000e
Kate <inthemist04@gmail.com>
2025-01-10 12:47:06 +0700

    делает первую часть домашнего задания

--- js/upload-image-form.js
+++ js/upload-image-form.js
@@ -1,3 +1,4 @@
+import { error, isHashtagValid } from './check-hashtag.js';
 import {isEscapeKey} from './util.js';
 
 const pageBody = document.querySelector('body');
@@ -8,13 +9,23 @@ const editorImgForm = uploadImgForm.querySelector('.img-upload__overlay');
 const uploadCancelBtn = editorImgForm.querySelector('#upload-cancel');
 const hashtagInput = uploadImgForm.querySelector('.text__hashtags');
 const commentInput = uploadImgForm.querySelector('.text__description');
+const submitButton = uploadImgForm.querySelector('.img-upload__submit');
+
+const pristine = new Pristine(uploadImgForm, {
+  classTo: 'img-upload__form',
+  errorClass: 'img-upload__field-wrapper--error',
+  errorTextParent: 'img-upload__field-wrapper',
+});
 
 const onUploadCancelBtnClick = () => {
   closeImgEditor();
 };
 
 const onDocumentKeyDown = (evt) => {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt)
+    && !evt.target.classList.contains('text__hashtags')
+    && !evt.target.classList.contains('text__description')
+  ) {
     evt.preventDefault();
     closeImgEditor();
   }
@@ -28,7 +39,7 @@ function closeImgEditor () {
   fileField.value = '';
 }
 
-const initUploadForm = () => {
+const openImgEditor = () => {
   uploadImgForm.addEventListener('change', () => {
     editorImgForm.classList.remove('hidden');
     pageBody.classList.add('modal-open');
@@ -37,5 +48,34 @@ const initUploadForm = () => {
   });
 };
 
-export {initUploadForm};
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикация...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+const onFormSubmit = () => {
+  uploadImgForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    if (isValid) {
+      blockSubmitButton();
+      unblockSubmitButton();
+    }
+  });
+};
+
+pristine.addValidator(hashtagInput, isHashtagValid, error, 2, false);
+
+function isCommentValid (value) {
+  return value.length <= 140;
+}
+
+pristine.addValidator(commentInput, isCommentValid, 'Комментарий не должен быть длиннее 140 символов');
+
+export {openImgEditor, onFormSubmit};
 
--- js/util.js
+++ js/util.js
@@ -14,4 +14,13 @@ const getRandomInteger = (a, b) => {
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomInteger, isEscapeKey};
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  if (num % 10 === 0 || num % 100 > 4 && num % 100 < 21) {
+    return genitivePlural;
+  }
+  return num % 10 === 1
+    ? nominative
+    : genitiveSingular;
+};
+
+export {getRandomInteger, isEscapeKey, numDecline};
