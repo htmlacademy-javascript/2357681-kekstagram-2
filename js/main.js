7b5cec9c2134437767e9f24ba7ff2ec9d56fdef8
Kate <inthemist04@gmail.com>
2025-01-29 19:38:30 +0700

    реализует механизм отправки формы с сообщениями для успеха и ошибки

--- js/main.js
+++ js/main.js
@@ -2,15 +2,25 @@ import {showAlert} from './util.js';
 import {createPhotoList} from './data.js';
 import {renderThumbnails} from './render-thumbnails.js';
 import {initClickListener} from './open-full-picture.js';
-import {openImgEditor} from './upload-image-form.js';
+import {openImgEditor, closeImgEditor, onFormSubmit, showFullSuccessMessage, showFullErrorMessage} from './upload-image-form.js';
 import { getData, sendData } from './api.js';
 
 const picturesDataList = createPhotoList();
 
-renderThumbnails(picturesDataList);
 initClickListener(picturesDataList);
 
 openImgEditor();
+
+onFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    closeImgEditor();
+    showFullSuccessMessage();
+  } catch {
+    showFullErrorMessage();
+  }
+});
+
 
 try {
   const data = await getData();
